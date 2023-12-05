4b1bc7518c3a90f163e970d2618e3c5371e6f51e
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-05 09:58:02 -0800

    Add dropdowns and form to MarketDisplay
    Co-authored-by: boboflofo <boboflofo@users.noreply.github.com>

--- src/components/MarketDisplay.js
+++ src/components/MarketDisplay.js
@@ -9,16 +9,60 @@ class MarketDisplay extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      month: props.month,
+      day: props.day
     };
   }
 
+  handleSubmit = () => {
+    // get value of monthDropdown and dayDropdown when submit is pressed
+    // would use this.setState({month: (value of monthDropdown), day: (value of dayDropdown)})
+  }
+
   render() {
+    let monthDropdown =
+      <select id="monthSelect">
+        <option>Jan</option>
+        <option>Feb</option>
+        <option>Mar</option>
+        <option>Apr</option>
+        <option>Jun</option>
+        <option>Jul</option>
+        <option>Aug</option>
+        <option>Sep</option>
+        <option>Oct</option>
+        <option>Nov</option>
+        <option>Dec</option>
+      </select>;
+
+    let dayDropdown =
+    <select id="daySelect">
+      <option>Sun</option>
+      <option>Mon</option>
+      <option>Tue</option>
+      <option>Wed</option>
+      <option>Thu</option>
+      <option>Fri</option>
+      <option>Sat</option>
+    </select>;
+    
+    let submitButton = <button onClick={this.handleSubmit}>Check Availability</button>;
+    
+    
+
+
+    
     return (
       <React.Fragment>
+        <form>
+        {monthDropdown}
+        {dayDropdown}
+        {submitButton}
+        </form>
+        
         <MonthSelector />
         <DaySelector />
-        <ProduceDisplay />
+        {/* <ProduceDisplay /> */}
       </React.Fragment>
     )
   }
