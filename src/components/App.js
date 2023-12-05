9b78927898ba0a3dc045098b0d6f9ca2c8830126
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-05 10:53:54 -0800

    Remove sample code from App.js
    Co-authored-by: boboflofo <boboflofo@users.noreply.github.com>
    Co-authored-by: Christopher Ross Davila <ChrisRDavila@users.noreply.github.com>

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,7 @@ import MarketDisplay from './MarketDisplay';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <MarketDisplay 
-        />
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MarketDisplay />
     </div>
   );
 }
